251ce84c937e2e94a0addd430bab4cf524221f38
vladimir.volodin <vladimir.volodin@unitbean.io>
2020-11-17 15:37:38 +0400

    исправил тейблвью

--- test/TableViewController.swift
+++ test/TableViewController.swift
@@ -17,7 +17,7 @@ struct Person {
     //если количество персон меньше пяти, то выводить кнопку(первой и добавить колво ячеек)
 class TableViewController: UITableViewController {
 
-    var numberOfCellsInSections = [2, 1]
+//    var numberOfCellsInSections = [2, 1]
     let headerTitles = ["My info", "My children's info"]
     var childsInfo = [Person]()
     var myInfo: Person?
@@ -32,11 +32,20 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return numberOfCellsInSections.count
+       return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfCellsInSections[section]
+        if section == 0{
+            return 2
+        }else{
+            if childsInfo.count < 5 {
+                return childsInfo.count + 1
+            }
+            else{
+                return childsInfo.count
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,35 +55,34 @@ class TableViewController: UITableViewController {
             let cell = tableView.dequeueReusableCell(withIdentifier: "InputInfoCell", for:indexPath) as? InputInfoCell
             return cell!
             
-        }else if indexPath.section == 1{
+        }else{
             
             if indexPath.row == 0 {
                 
-                if numberOfCellsInSections[1] < 6 {
+                if childsInfo.count < 5 {
                     let cell = tableView.dequeueReusableCell(withIdentifier: "AddChildrenInfoCell", for:indexPath) as? AddChildrenInfoCell
                     return cell!
                 }else{
-                    let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as? MyChildrenInfoCell
-                                        cell?.nameLabel.text = childsInfo[indexPath.row].name
-                                        cell?.ageLabel.text = childsInfo[indexPath.row].age
-                    return cell!
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as! MyChildrenInfoCell
+                    cell.nameLabel.text = childsInfo[indexPath.row].name
+                    cell.ageLabel.text = childsInfo[indexPath.row].age
+                    return cell
                 }
             }
-        
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as! MyChildrenInfoCell
-            cell.nameLabel.text = childsInfo[indexPath.row - 1].name
-            cell.ageLabel.text = childsInfo[indexPath.row - 1].age
-            return cell
+            if childsInfo.count < 5{
+                let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as! MyChildrenInfoCell
+                cell.nameLabel.text = childsInfo[indexPath.row - 1].name
+                cell.ageLabel.text = childsInfo[indexPath.row - 1].age
+                return cell
+            }else{
+                let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as! MyChildrenInfoCell
+                cell.nameLabel.text = childsInfo[indexPath.row].name
+                cell.ageLabel.text = childsInfo[indexPath.row].age
+                return cell
+            }
         }
-        
-        //хаглушка
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyChildrenInfoCell", for:indexPath) as! MyChildrenInfoCell
-        cell.nameLabel.text = childsInfo[indexPath.row].name
-        cell.ageLabel.text = childsInfo[indexPath.row].age
-        return cell
     }
-
+    
     
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -85,7 +93,7 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.section == 1 && indexPath.row == 0 && numberOfCellsInSections[1] < 6 {
+        if indexPath.section == 1 && indexPath.row == 0 && childsInfo.count < 5 {
             let alert = UIAlertController(title: "Type child's info", message: "", preferredStyle: .alert)
 
             alert.addTextField { (textField) in
@@ -100,14 +108,12 @@ class TableViewController: UITableViewController {
                 let nameTextField = alert?.textFields![0]
                 let ageTextField = alert?.textFields![1]
                 self.childsInfo.append(Person(name: (nameTextField?.text)!, age: (ageTextField!.text!)))
-                self.numberOfCellsInSections[1] += 1
+//                self.numberOfCellsInSections[1] += 1
                 tableView.reloadData()
                 print(self.childsInfo)
             }))
             
             self.present(alert, animated: true, completion: nil)
-            
-   
         }
     }
 
